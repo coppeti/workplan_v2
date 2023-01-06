d99b896d1560b35195bb1441aa6492597eb8dd3c
Etienne Coppel <eco@ik.me>
2023-01-06 23:34:59 +0100

    Widget-tweaks uninstalled, all form with crispy

--- accounts/forms.py
+++ accounts/forms.py
@@ -61,55 +61,81 @@ class UserForgotPasswordForm(PasswordResetForm):
     
     
 class EditUserForm(forms.ModelForm):
-    
-    class Meta:
-        model = CustomUser
-        fields = ['first_name', 'last_name', 'username', 'email', 'birthday', 'is_active', 'is_staff', 'is_superuser']
-        labels= {
-            'first_name': 'Vorname',
-            'last_name': 'Nachname',
-            'username': 'Pseudo',
-            'email': 'Email-Adresse',
-            'birthday': 'Geburtsdatum',
-            'is_active': 'Ist aktiv',
-            'is_staff': 'Ist Administrator',
-            'is_superuser': 'Ist Superuser',
-        }
-        
-        widgets = {
-            'birthday': forms.NumberInput(
-                attrs={'type': 'date'}
-            )
-        }
-
-
-class EditProfileForm(forms.ModelForm):
     first_name = forms.CharField(required=True,
                                  max_length=50,
-                                 label = 'Vorname',
+                                 label='Vorname',
                                  validators=[RegexValidator(r'^[a-zA-ZÀ-ÿ-\'\s]*$',
                                                             message="Verwenden Sie nur Buchstaben !")],
                                  widget=forms.TextInput(attrs={'style': 'text-transform: capitalize'})
                                  )
     last_name = forms.CharField(required=True,
                                 max_length=50,
-                                label = 'Nachname',
+                                label='Nachname',
                                 validators=[RegexValidator(r'^[a-zA-ZÀ-ÿ-\'\s]*$',
                                                             message="Verwenden Sie nur Buchstaben !")],
                                 widget=forms.TextInput(attrs={'style': 'text-transform: capitalize'})
                                  )
     username = forms.CharField(required=True,
                                max_length=30,
-                               label = 'Pseudo',
+                               label='Pseudo',
                                validators=[RegexValidator(r'^[a-z0-9]{3,}$',
                                                           message='Gib mindestens 3 Kleinbuchstaben und/oder Zahlen ein')])
     
     birthday = forms.DateField(required=True,
-                               label = 'Geburtsdatum',
+                               label='Geburtsdatum',
                                widget=forms.NumberInput(attrs={'type': 'date'}))
 
     email = forms.EmailField(required=True,
-                             label = 'Email-Adresse',
+                             label='Email-Adresse',
+                             validators=[RegexValidator(r'^([A-Za-z0-9_.+-])+\@(([A-Za-z0-9-])+\.)+([A-Za-z0-9]{2,4})+$',
+                                                        message='Gib eine gültige E-Mail-Adresse ein !')],
+                             widget=forms.TextInput(attrs={'style': 'text-transform: lowercase'})
+                             )
+    
+    is_active = forms.BooleanField(required=False,
+                                   label = 'Aktiver Benutzer',
+                                   )
+    
+    is_staff = forms.BooleanField(required=False,
+                                  label='Staff Benutzer',
+                                  )
+    
+    is_superuser = forms.BooleanField(required=False,
+                                      label='Superuser',
+                                      )
+    
+    class Meta:
+        model = CustomUser
+        fields = ['first_name', 'last_name', 'username', 'email', 'birthday', 'is_active', 'is_staff', 'is_superuser']
+
+
+class EditProfileForm(forms.ModelForm):
+    first_name = forms.CharField(required=True,
+                                 max_length=50,
+                                 label='Vorname',
+                                 validators=[RegexValidator(r'^[a-zA-ZÀ-ÿ-\'\s]*$',
+                                                            message="Verwenden Sie nur Buchstaben !")],
+                                 widget=forms.TextInput(attrs={'style': 'text-transform: capitalize'})
+                                 )
+    last_name = forms.CharField(required=True,
+                                max_length=50,
+                                label='Nachname',
+                                validators=[RegexValidator(r'^[a-zA-ZÀ-ÿ-\'\s]*$',
+                                                            message="Verwenden Sie nur Buchstaben !")],
+                                widget=forms.TextInput(attrs={'style': 'text-transform: capitalize'})
+                                 )
+    username = forms.CharField(required=True,
+                               max_length=30,
+                               label='Pseudo',
+                               validators=[RegexValidator(r'^[a-z0-9]{3,}$',
+                                                          message='Gib mindestens 3 Kleinbuchstaben und/oder Zahlen ein')])
+    
+    birthday = forms.DateField(required=True,
+                               label='Geburtsdatum',
+                               widget=forms.NumberInput(attrs={'type': 'date'}))
+
+    email = forms.EmailField(required=True,
+                             label='Email-Adresse',
                              validators=[RegexValidator(r'^([A-Za-z0-9_.+-])+\@(([A-Za-z0-9-])+\.)+([A-Za-z0-9]{2,4})+$',
                                                         message='Gib eine gültige E-Mail-Adresse ein !')],
                              widget=forms.TextInput(attrs={'style': 'text-transform: lowercase'})
@@ -118,9 +144,3 @@ class EditProfileForm(forms.ModelForm):
     class Meta:
         model = CustomUser
         fields = ['first_name', 'last_name', 'username', 'email', 'birthday']
-        
-        
-# class UserPasswordChangeForm(PasswordChangeForm):
-    
-#     class Meta:
-#         model = CustomUser
