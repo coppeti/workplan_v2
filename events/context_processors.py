e66103637231291b612f8a5d883342a1b865a602
Etienne Coppel <eco@ik.me>
2023-04-17 13:30:00 +0200

    sidebar refactoring

--- events/context_processors.py
+++ events/context_processors.py
@@ -5,6 +5,32 @@ from .models import Events
 
 def sidebar_events(request):
     today = datetime.now()
-    return {'s_events': Events.objects.filter(confirmed='True',
-                                              date_start__lte=today.strftime('%Y-%m-%d'),
-                                              date_stop__gte=today.strftime('%Y-%m-%d')).order_by('id')}
+    return {'dispatcher_event': Events.objects.filter(activity_id__name='Dispatcher',
+                                                      is_active=True,
+                                                      confirmed=True,
+                                                      date_start__lte=today.strftime('%Y-%m-%d'),
+                                                      date_stop__gte=today.strftime('%Y-%m-%d')),
+            'pikett_event': Events.objects.filter(activity_id__name='Pikett',
+                                                  is_active=True,
+                                                  confirmed=True,
+                                                  date_start__lte=today.strftime('%Y-%m-%d'),
+                                                  date_stop__gte=today.strftime('%Y-%m-%d')),
+            'vacation_event': Events.objects.filter(activity_id__name='Ferien',
+                                                    is_active=True,
+                                                    confirmed=True,
+                                                    date_start__lte=today.strftime('%Y-%m-%d'),
+                                                    date_stop__gte=today.strftime('%Y-%m-%d')).order_by('id'),
+            'compensation_event': Events.objects.filter(activity_id__name='Frei/Kompensation',
+                                                        is_active=True,
+                                                        confirmed=True,
+                                                        date_start__lte=today.strftime('%Y-%m-%d'),
+                                                        date_stop__gte=today.strftime('%Y-%m-%d')).order_by('id'),
+            'other_event': Events.objects.filter(is_active=True,
+                                                 confirmed=True,
+                                                 date_start__lte=today.strftime('%Y-%m-%d'),
+                                                 date_stop__gte=today.strftime('%Y-%m-%d'),).exclude(
+                                                     activity_id__name='Dispatcher').exclude(
+                                                         activity_id__name='Pikett').exclude(
+                                                             activity_id__name='Ferien').exclude(
+                                                                 activity_id__name='Frei/Kompensation').order_by('id')
+            }
